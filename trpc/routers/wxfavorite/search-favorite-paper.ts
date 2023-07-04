323685381f66fd179eeebdf27e913f2f9dfbab9a
朱炜炜 <itweiboy@163.com>
2023-07-04 17:46:57 +0800

    BUG修改

--- trpc/routers/wxfavorite/search-favorite-paper.ts
+++ trpc/routers/wxfavorite/search-favorite-paper.ts
@@ -9,15 +9,12 @@ const scarchFavoritePaper = publicProcedure
     .input(searchSchema)
     .query(async ({input, ctx}) => {
         let { userId,openId,favoriteId,pageNum,pageSize } = input;
-        if (favoriteId == 0) {
-            favoriteId = null
-        }
         return  await prisma.favoriteDetails.findMany({
             take: pageSize, // 指定每页要获取的结果数量
             skip: (pageNum - 1) * pageSize, // 根据当前页码计算要跳过的结果数量
             where: {
                 ...(favoriteId && { favoriteId }),
-                weChatUserId: userId,
+                weChatUserId: userId !== null ? userId : undefined,
                 openId: openId
             },
             include: {
